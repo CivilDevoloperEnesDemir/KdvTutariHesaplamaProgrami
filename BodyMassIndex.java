cd729ec83cbff316e6bfb4488354e03ec007409c
CivilDevoloperEnesDemir <114176807+CivilDevoloperEnesDemir@users.noreply.github.com>
2022-09-24 16:45:47 +0300

    BodyMassIndexR-1

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 public class BodyMassIndex {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        double height , weight , bms ;
+        double height , weight , bmi ;
         // Formula = weight / height * height
         System.out.print("What is your body weight as kilogram : ");
         weight = inp.nextDouble();
         System.out.print("What is your height as meter : ");
         height = inp.nextDouble();
 
-        bms = weight / Math.pow(height,2);
+        bmi = weight / Math.pow(height,2);
 
-        System.out.print("Your body mass index is : "+bms);
+        System.out.print("Your body mass index is : "+bmi);
 
 
 
