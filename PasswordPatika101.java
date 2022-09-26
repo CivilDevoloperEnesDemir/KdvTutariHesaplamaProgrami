220801b58afcced7807a5333fdfbae1805f61524
CivilDevoloperEnesDemir <114176807+CivilDevoloperEnesDemir@users.noreply.github.com>
2022-09-26 17:33:18 +0300

    Update PasswordPatika101.java

--- PasswordPatika101.java
+++ PasswordPatika101.java
@@ -1,5 +1,6 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -17,11 +18,16 @@ public class Main {
 
         if(kullaniciadi.equals("java") && sifre.equals("patika")){
             System.out.println("Bilgileriniz doğru giriş başarılı !");
-        }else if(!kullaniciadi.equals("java") || !sifre.equals("patika")){
+            
+        }
+        else if(!kullaniciadi.equals("java") || !sifre.equals("patika")){
+            
             System.out.println("Bilgileriniz yanlış.");
             System.out.println("Yeni kullanıcı adı ve şifre oluşturmak ister misiniz?\n1-Evet\n2-Hayır");
             value = inp.nextInt();
+            
             if(value==1){
+                
                 Scanner input = new Scanner(System.in);
                 System.out.print("Yeni Kullanıcı Adı : ");
                 String cvpb = input.nextLine();
@@ -29,14 +35,17 @@ public class Main {
                 String cvpc = input.nextLine();
 
                 if(!cvpb.equals("java") && !cvpc.equals("patika")) {
+                    
                     System.out.println("İşlem başarılı");
                 }
-
                 if(cvpb.equals("java") && cvpc.equals("patika")){
+                    
                     System.out.println("Girilen bilgiler eski bilgilerle aynı olamaz !");
                     System.out.println("Lütfen farklı bir kullanıcı adı ve şifre seçin.\nDevam etmek için 1 numaralı İşlemi\nSonlandırmak için 2 numaralı işlemi seçin\n1-Devam et\n2-Sonlandır");
                     value = inp.nextInt();
+                    
                     if(value==1) {
+                        
                         Scanner as = new Scanner(System.in);
                         System.out.print("Yeni Kullanıcı Adı : ");
                         cvpb = as.nextLine();
@@ -45,16 +54,18 @@ public class Main {
                         cvpc = as.nextLine();
                         System.out.println("İşlem Başarılı !");
 
-                    }if(value==2){
+                    }
+                    if(value==2){
+                        
                         System.out.println("İşleminiz sonlandı");
 
                     }
 
                 }
 
-            }if(value==2){
+            }
+            if(value==2){
                 System.out.println("İşleminiz sonlandı");
-
             }
         }
 
